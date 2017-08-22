74d04e000e66e8853e426a61c3c68192b4d8d7d0
yjyao <yuanjun.yao@duke.edu>
2017-08-22 12:04:10 -0400

    sdx with rest api wororks now

--- SDX-Client-ExoGENI/scripts/sdxclient.sh
+++ SDX-Client-ExoGENI/scripts/sdxclient.sh
@@ -3,5 +3,5 @@
 #./target/appassembler/bin/SafeSdxExample ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" postbootfour fournodes
 #./target/appassembler/bin/SafeSdxServer ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" $1 $2 $3 $4 $5 $6 $7
 #./target/appassembler/bin/SafeSdxClient ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" $1 $2 $3 $4 $5 $6 $7
-./target/appassembler/bin/SafeSdxClient $1 $2
+./SDX-Client-ExoGENI/target/appassembler/bin/SdxExoGENIClient $1 $2
 
