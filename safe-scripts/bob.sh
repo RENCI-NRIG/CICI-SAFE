c19b5a8d3e91ee4521202558aa555c3cbcc5a3ab
yjyao <yuanjun.yao@duke.edu>
2017-08-31 11:13:45 -0400

    comments in safe scripts

--- safe-scripts/bob.sh
+++ safe-scripts/bob.sh
@@ -6,14 +6,19 @@
 #key_p6: KXwvxF_rWupThUEAKwmkMTuhV8X-hqZXOAtMkWBFapc
 
 SAFESERVER_BOB=128.194.6.137
+
 curl  -v -X POST http://$SAFESERVER_BOB:7777/postIdSet -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [\"bob\"] }"
+
 curl  -v -X POST http://$SAFESERVER_BOB:7777/postSubjectSet -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [] }"
 
 
-curl  -v -X POST http://$SAFESERVER_BOB:7777/postStitchPolicy -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [] }"
+#Bob post her access control policy, comment it out if we don't allow alice to talk to any other
 curl  -v -X POST http://$SAFESERVER_BOB:7777/postACLPolicy -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [] }"
-curl  -v -X POST http://$SAFESERVER_BOB:7777/postConnectivityPolicy -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [] }"
-curl  -v -X POST http://$SAFESERVER_BOB:7777/postOwnPrefixPolicy -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [] }"
+
+#Bob endorse a project authority
 curl  -v -X POST http://$SAFESERVER_BOB:7777/postEndorsePA -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [\"weQ8OFpXWhIB1AMzKX2SDJcxT738VdHCcl7mFlvOD24\"] }"
+
+#Bob put the tocken for ip delegation and project membership delegation in her subject set
 curl -v -X POST http://$SAFESERVER_BOB:7777/updateSubjectSet -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [\"SV51XV5zIlONA255wethUSJRNTxG8Lh-kaYP9PZEOxU\"] }"
+
 curl -v -X POST http://$SAFESERVER_BOB:7777/updateSubjectSet -H "Content-Type: application/json" -d "{ \"principal\": \"bob\", \"otherValues\": [\"HLSqBJpzAnD42qqmwI2qioNHo_7RDheKy_gS5_mAmM4\"] }"
