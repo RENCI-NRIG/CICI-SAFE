2ace8c38e860a6a956490bafe420bbb35c5e76d1
Yuanjun Yao <yy126@duke.edu>
2017-11-09 03:31:51 -0500

    manual dynamic link. next: request with bandwidth and traffic rate limitation

--- SDX-Client-ExoGENI/SDX_ExoGENI_Client/src/main/java/safe/exogeni/client/SdxExogeniClient.java
+++ SDX-Client-ExoGENI/SDX_ExoGENI_Client/src/main/java/safe/exogeni/client/SdxExogeniClient.java
@@ -96,7 +96,7 @@ public class SdxExogeniClient extends SliceCommon {
     sctx = new SliceAccessContext<>();
     try {
       SSHAccessTokenFileFactory fac;
-      fac = new SSHAccessTokenFileFactory(sshkey+".ssh", false);
+      fac = new SSHAccessTokenFileFactory(sshkey+".pub", false);
       SSHAccessToken t = fac.getPopulatedToken();
       sctx.addToken("root", "root", t);
       sctx.addToken("root", t);
