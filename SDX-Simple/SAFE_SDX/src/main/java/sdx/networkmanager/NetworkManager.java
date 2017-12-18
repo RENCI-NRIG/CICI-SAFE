3cb05eefa6bb1b73644e4d2c2a1dc19ae4ecd2c5
Yuanjun Yao <yjyao@cs.duke.edu>
2017-12-18 00:36:18 -0500

    tmp

--- SDX-Simple/SAFE_SDX/src/main/java/sdx/networkmanager/NetworkManager.java
+++ SDX-Simple/SAFE_SDX/src/main/java/sdx/networkmanager/NetworkManager.java
@@ -92,6 +92,29 @@ public class NetworkManager {
     addRouter(routerid, dpid, numInterfaces, mip);
   }
 
+  public void newLink(String ipa, String ra, String gw,String controller) {
+    logger.debug("RoutingManager: new link "+ra+ipa);
+    System.out.println("new stitch "+ra +" gateway:"+ipa);
+    addLink(ipa,ra, gw);
+    String dpid= getRouter(ra).getDPID();
+    String cmd[] = addrCMD(ipa,dpid,controller);
+    HttpUtil.postJSON(cmd[0],new JSONObject(cmd[1]));
+    addEntry_HashList(sdncmds,dpid,cmd);
+  }
+
+  public void newLink(String ipa, String ra, String ipb, String rb, String controller){
+    logger.debug("RoutingManager: new link "+ra+ipa+rb+ipb);
+    System.out.println("new link  ra "+ra+" ipa "+ipa+ " rb "+rb +" ipb "+ipb);
+    addLink(ipa,ra,ipb,rb);
+    String dpid=getDPID(ra);
+    String[] cmd = addrCMD(ipa,dpid,controller);
+    HttpUtil.postJSON(cmd[0],new JSONObject(cmd[1]));
+    addEntry_HashList(sdncmds,dpid,cmd);
+    dpid=getDPID(rb);
+    cmd = addrCMD(ipb,dpid,controller);
+    HttpUtil.postJSON(cmd[0],new JSONObject(cmd[1]));
+    addEntry_HashList(sdncmds,dpid,cmd);
+  }
   public void newLink(String ipa, String ra, String controller) {
     logger.debug("RoutingManager: new link " + ra + ipa);
     addLink(ipa, ra);
