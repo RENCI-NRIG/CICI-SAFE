b8bb98768736b2f85233d9c65c461b6afbe5185f
Yuanjun Yao <yjyao@cs.duke.edu>
2018-02-15 23:03:15 -0500

    test service, config file

--- SDX-Simple/SAFE_SDX/src/main/java/sdx/core/RestService.java
+++ SDX-Simple/SAFE_SDX/src/main/java/sdx/core/RestService.java
@@ -56,7 +56,7 @@ public class RestService {
   @Path("/stitchchameleon")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
-  public String stitchRequest(StitchChameleon sr) {
+  public String stitchChameleon(StitchChameleon sr) {
     logger.debug("got chameleon stitch request: \n" + sr.toString());
     System.out.println(String.format("got chameleon stitch request from %s", sr.ckeyhash));
     String res = SdxServer.sdxManager.stitchChameleon(sr.sdxslice, sr.sdxnode, sr.ckeyhash, sr
@@ -70,7 +70,7 @@ public class RestService {
   @Path("/notifyprefix")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
-  public String stitchRequest(PrefixNotification pn) {
+  public String notifyPrefix(PrefixNotification pn) {
     logger.debug("got sittch request");
     String res = SdxServer.sdxManager.notifyPrefix(pn.dest, pn.gateway, pn.router, pn.customer);
     logger.debug(res);
