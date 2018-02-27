570327a2aa50aa2c02515c25b6bc7ed5d9d23e02
Yuanjun Yao <yjyao@cs.duke.edu>
2018-02-27 17:17:13 -0500

    remove SDX-orig

--- SDX-orig/SAFE_SDX/ahab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-##./target/appassembler/bin/SafeSdxExample ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" postbootfour fournodes
-#./target/appassembler/bin/SafeSdxExample ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" $1 $2 $3 $4 $5 $6 $7 $8 $9
-./target/appassembler/bin/SafeSdxExample $1 $2 $3
-
--- SDX-orig/safe/super-safe/safe-apps/safe-network/routing/test.sh
+++
@@ -1,4 +0,0 @@
-curl  -v -X POST http://152.3.136.36:7777/posttestIP -H "Content-Type: application/json" -d "{ \"principal\": \"XLajumPCpZWScMJUoxrApXMCxRllpAVnFIwzv7CFpsU\", \"otherValues\": [] }"
-
-curl  http://152.3.136.36:7777/ipInRange -H "Content-Type: application/json" -d "{ \"principal\": \"XLajumPCpZWScMJUoxrApXMCxRllpAVnFIwzv7CFpsU\", \"otherValues\": [\"ipv4\\\"152.3.2.1/24\\\"\",\"ipv4\\\"152.3.2.1/16\\\"\"] }"
-
