3efacccda46387e0bd37668a379587397b57d8a7
Yuanjun Yao <yjyao@cs.duke.edu>
2018-04-14 17:07:28 -0400

    update scritps

--- SDX-Simple/scripts/cnert-client.sh
+++ SDX-Simple/scripts/cnert-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SDX=test-fl-yao
+SDX=sdx-yaoyj11
 echo "stitch clients"
 ./scripts/sdx_exogeni_client.sh -c client-config/c1.conf -e "stitch CNode0 ${SDX} c0"
 ./scripts/sdx_exogeni_client.sh -c client-config/c2.conf -e "stitch CNode0 ${SDX} c1"
@@ -15,5 +15,5 @@ echo "advertise ip prefixes, users can parse the gateway ip address from the out
 
 "echo request for connections"
 
-./scripts/sdx_exogeni_client.sh -c client-config/c1-ufl.conf -e  "link 192.168.10.1/24 192.168.20.1/24"
-./scripts/sdx_exogeni_client.sh -c client-config/c3-ufl.conf -e  "link 192.168.30.1/24 192.168.40.1/24"
+./scripts/sdx_exogeni_client.sh -c client-config/c1.conf -e  "link 192.168.10.1/24 192.168.20.1/24"
+./scripts/sdx_exogeni_client.sh -c client-config/c3.conf -e  "link 192.168.30.1/24 192.168.40.1/24"
--- SDX-Simple/scripts/cnert-slices.sh
+++ SDX-Simple/scripts/cnert-slices.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "If there is existing slices, delete them"
-./scripts/createslice.sh -c config/cnert-fl.conf -d
-./scripts/createslice.sh -c client-config/c1-ufl.conf -d
-./scripts/createslice.sh -c client-config/c3-ufl.conf -d
-./scripts/createslice.sh -c client-config/c2-unf.conf -d
-./scripts/createslice.sh -c client-config/c4-unf.conf -d
+./scripts/createslice.sh -c config/sdx.conf -d
+./scripts/createslice.sh -c client-config/c1.conf -d
+./scripts/createslice.sh -c client-config/c3.conf -d
+./scripts/createslice.sh -c client-config/c2.conf -d
+./scripts/createslice.sh -c client-config/c4.conf -d
 
 echo "creating slices"
-./scripts/createslice.sh -c config/cnert-fl.conf
-./scripts/createclientslice.sh -c client-config/c1-ufl.conf
-./scripts/createclientslice.sh -c client-config/c3-ufl.conf
-./scripts/createclientslice.sh -c client-config/c2-unf.conf
-./scripts/createclientslice.sh -c client-config/c4-unf.conf
+./scripts/createslice.sh -c config/sdx.conf
+./scripts/createclientslice.sh -c client-config/c1.conf
+./scripts/createclientslice.sh -c client-config/c3.conf
+./scripts/createclientslice.sh -c client-config/c2.conf
+./scripts/createclientslice.sh -c client-config/c4.conf
 
