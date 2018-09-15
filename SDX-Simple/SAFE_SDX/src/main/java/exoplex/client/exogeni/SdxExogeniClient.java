dccf6803f1c6b6b719a89d9b0266fab8bb9a16c9
Yuanjun Yao <yjyao@cs.duke.edu>
2018-09-15 12:24:07 -0400

    fix exogeni cilent bug

--- SDX-Simple/SAFE_SDX/src/main/java/exoplex/client/exogeni/SdxExogeniClient.java
+++ SDX-Simple/SAFE_SDX/src/main/java/exoplex/client/exogeni/SdxExogeniClient.java
@@ -77,9 +77,9 @@ public class SdxExogeniClient extends SliceCommon{
 
   public void run(String[] args) {
     try {
-      SafeSlice s = SafeSlice.loadManifestFile(sliceName, pemLocation, keyLocation, controllerUrl);
-      if(s.getResourceByName("safe-server")!= null) {
-        setSafeServerIp(s.getComputeNode("safe-server").getManagementIP());
+      serverSlice = SafeSlice.loadManifestFile(sliceName, pemLocation, keyLocation, controllerUrl);
+      if(serverSlice.getResourceByName("safe-server")!= null) {
+        setSafeServerIp(serverSlice.getComputeNode("safe-server").getManagementIP());
       }else {
         setSafeServerIp(conf.getString("config.safeserver"));
       }
