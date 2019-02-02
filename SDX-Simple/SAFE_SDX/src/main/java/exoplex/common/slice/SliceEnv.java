158b2a4b297b76bc87185924970f7a6d867df44a
Yuanjun Yao <yjyao@cs.duke.edu>
2019-02-02 18:13:48 -0500

    Revert "use ubuntu17" and resolve conflicts

--- SDX-Simple/SAFE_SDX/src/main/java/exoplex/common/slice/SliceEnv.java
+++ SDX-Simple/SAFE_SDX/src/main/java/exoplex/common/slice/SliceEnv.java
@@ -1,9 +1,9 @@
 package exoplex.common.slice;
 
 public class SliceEnv {
-  public static final String OVSVersion = NodeBase.UBUNTU17;
-  public static final String SafeVMVersion = NodeBase.U14Docker;
-  public static final String CustomerVMVersion = NodeBase.UBUNTU14;
-  //public static final String OFP = "-O OpenFlow15";
   public static final String OFP = "-O OpenFlow13";
+  public static final String VMVersion = NodeBase.UBUNTU14;
+  public static final String SafeVMVersion = "Ubuntu 14.04 Docker";
+  public static final String CustomerVMVersion = "Ubuntu 14.04";
+  //public static final String OFP = "";
 }
