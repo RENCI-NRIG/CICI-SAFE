c76ec154716a8d433d1d605700f562b2c9bb9574
Yuanjun Yao <yjyao@cs.duke.edu>
2019-03-29 17:23:13 -0400

    remove interface from a bridge after unstitching

--- SDX-Simple/SAFE_SDX/src/main/resources/scripts/ovsbridge.sh
+++ SDX-Simple/SAFE_SDX/src/main/resources/scripts/ovsbridge.sh
@@ -11,7 +11,9 @@ if [[ $string == *"br0"* ]]; then
     :
 else
   ovs-vsctl add-br br0
-  ovs-vsctl set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15
+  #ovs-vsctl set Bridge br0 protocols=OpenFlow13,OpenFlow15
+  #ovs-vsctl set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow15
+  ovs-vsctl set Bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
   ovs-vsctl set-manager ptcp:6632
   ovs-vsctl set-controller br0 tcp:$1
 fi
@@ -21,7 +23,10 @@ manageif=$(ifconfig -a| grep -B1 "inet 10.\|inet addr:10." | awk '$1!="inet" && 
 interfaces=$(ifconfig -a|grep "ens\|eth"|grep -v "$manageif"|sed 's/[ :\t].*//;/^$/d')
 
 brinterfaces=$(ovs-ofctl show br0)
+#when using -O OpenFlow13, it failed to show interfaces in ovs-ofctl show br0
 vsinterfaces=$(ovs-vsctl show)
+
+vsis=$(ovs-vsctl show|grep -E "Port.*(eth|ens)" |sed 's/Port.*"\(.*\)".*/\1/')
 
 newport=0
 while read -r line; do
@@ -38,7 +43,18 @@ while read -r line; do
     fi
   fi
 done <<< "$interfaces"
-dpid=$(ovs-ofctl show br0 | grep "dpid:" |cut -d: -f3)
+
+
+while read -r line; do
+  if [[ $interfaces == *"$line"* ]]; then
+  :
+  else
+    ovs-vsctl del-port br0 $line
+  fi
+done <<< "$vsis"
+
+dpid=$(ovs-ofctl show br0| grep "dpid:" |cut -d: -f3)
 echo $dpid
+
 
 
