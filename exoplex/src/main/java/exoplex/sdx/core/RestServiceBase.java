14acd5763de4ca60dbf692f409acecae11c8e84d
Komal Thareja <kthar10@renci.org>
2019-11-15 19:39:12 -0500

    added logging, error handling and updated local data structures

--- exoplex/src/main/java/exoplex/sdx/core/RestServiceBase.java
+++ exoplex/src/main/java/exoplex/sdx/core/RestServiceBase.java
@@ -14,6 +14,8 @@ import javax.ws.rs.core.UriInfo;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @Path("sdx")
 public class RestServiceBase {
@@ -52,15 +54,21 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
   public String processAdminCmd(@Context UriInfo uriInfo, AdminCmd cmd) {
+    logger.info("STARTED==============================");
+    String result = null;  
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.debug(String.format("%s got stitch request %s", exoSdxManager.getSliceName(), cmd));
     try {
-      return exoSdxManager.adminCmd(cmd.operation, cmd.params);
+      result = exoSdxManager.adminCmd(cmd.operation, cmd.params);
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
@@ -68,15 +76,21 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
   public String processFlowPacketIn(@Context UriInfo uriInfo, Flow packetin) {
+    logger.info("STARTED==============================");
+    String result = null;  
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.debug(String.format("%s got packet in %s", exoSdxManager.getSliceName(), packetin));
     try {
-      return exoSdxManager.processPacketIn(packetin.src, packetin.dest);
+      result = exoSdxManager.processPacketIn(packetin.src, packetin.dest);
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
@@ -84,36 +98,50 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
   public String receiveBgpAdvertise(@Context UriInfo uriInfo, RouteAdvertise routeAdvertise) {
+    logger.info("STARTED==============================");
+    String result = null;      
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.info(translatePids(String.format("%s got bgp advertisement %s", exoSdxManager.getSliceName(), routeAdvertise)));
     try {
-      return exoSdxManager.processBgpAdvertise(routeAdvertise);
+      result = exoSdxManager.processBgpAdvertise(routeAdvertise);
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @GET
   @Path("/getpid")
   @Produces(MediaType.TEXT_PLAIN)
   public String getPid(@Context UriInfo uriInfo) {
+    logger.info("STARTED==============================");
+    String result = null;      
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.debug(String.format(" %s got bgp pid request", exoSdxManager.getSliceName()));
     try {
-      return exoSdxManager.getPid();
+      result = exoSdxManager.getPid();
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
   @Path("/flow")
   @Produces(MediaType.TEXT_PLAIN)
   public String getFLow(@Context UriInfo uriInfo, Flow flow) {
+    logger.info("STARTED==============================");
+    String result = null;      
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.debug(String.format(" %s got flow request", exoSdxManager.getSliceName()));
@@ -127,11 +155,15 @@ public class RestServiceBase {
     }
     patterns.add(pattern);
     try {
-      return exoSdxManager.logFlowTables(patterns, new ArrayList<>());
+      result = exoSdxManager.logFlowTables(patterns, new ArrayList<>());
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
@@ -139,15 +171,21 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
   public String receivePolicyAdvertise(@Context UriInfo uriInfo, PolicyAdvertise policyAdvertise) {
+    logger.info("STARTED==============================");
+    String result = null;      
     logger.debug(uriInfo.getBaseUri());
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.info(translatePids(String.format("%s got policy advertisement %s", exoSdxManager.getSliceName(), policyAdvertise)));
     try {
-      return exoSdxManager.processPolicyAdvertise(policyAdvertise);
+      result = exoSdxManager.processPolicyAdvertise(policyAdvertise);
     } catch (Exception e) {
-      e.printStackTrace();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
@@ -155,16 +193,21 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
   public String connectionRequest(@Context UriInfo uriInfo, ConnectionRequest sr) {
+    logger.info("STARTED==============================");
+    String result = null;      
     SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
     logger.debug(String.format("%s got link request between %s and %s", exoSdxManager.getSliceName(), sr.self_prefix, sr.target_prefix));
     try {
-      String res = exoSdxManager.connectionRequest(sr.self_prefix, sr.target_prefix, sr.bandwidth);
-      return res;
+      result = exoSdxManager.connectionRequest(sr.self_prefix, sr.target_prefix, sr.bandwidth);
     } catch (Exception e) {
       e.printStackTrace();
-      exoSdxManager.unlockSlice();
-      return e.getMessage();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = e.getMessage();
     }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   @POST
@@ -172,10 +215,23 @@ public class RestServiceBase {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public NotifyResult notifyPrefix(@Context UriInfo uriInfo, PrefixNotification pn) {
-    SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
-    logger.debug(String.format("%s got notifyprefix %s", exoSdxManager.getSliceName(), pn.toString()));
-    NotifyResult res = exoSdxManager.notifyPrefix(pn.dest, pn.gateway, pn.customer);
-    return res;
+    logger.info("STARTED==============================");
+    NotifyResult result = null;      
+    try {
+      SdxManagerBase exoSdxManager = sdxManagerMap.get(uriInfo.getBaseUri().getPort());
+      logger.debug(String.format("%s got notifyprefix %s", exoSdxManager.getSliceName(), pn.toString()));
+      result = exoSdxManager.notifyPrefix(pn.dest, pn.gateway, pn.customer);
+    } catch (Exception e) {
+      e.printStackTrace();
+      StringWriter errors = new StringWriter();
+      e.printStackTrace(new PrintWriter(errors));
+      logger.error(errors);
+      result = new NotifyResult();
+      result.result = false;
+      result.message = e.getMessage();
+    }
+    logger.info("COMPLETED==============================");
+    return result;
   }
 
   private String translatePids(String s) {
